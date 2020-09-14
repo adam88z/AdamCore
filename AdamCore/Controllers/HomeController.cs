1b3906803b4a301d7fd6c2f097d023f1d1602bdc
adam88z <arosr79@gmail.com>
2020-09-14 17:29:12 -0400

    Initial check-in of module AdamCore

---
+++ AdamCore/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdamCore.Models;
+
+namespace AdamCore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        public String[] ImageUploadGet()
+        {
+
+            string[] images = { "images/orb.jpg", "images/maddi_web2.jpg", "images/tuccs.jpg" };
+
+            //string x = "images/cool-bridge.jpg";
+            //string y = "images/oakland_street.jpg";
+
+            return images;
+        }
+
+
+
+    }
+}
