46299ad41a08ff0bc3d372e6c49af5e26904a29f
adam james <arosr79@gmail.com>
2020-09-23 13:52:50 -0400

    Started to add a html helper to the Index page, an a tag that call an action from the home controller.

--- AdamCore/Controllers/HomeController.cs
+++ AdamCore/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace AdamCore.Controllers
 
         public IActionResult Index()
         {
+
             return View();
         }
 
@@ -35,7 +36,12 @@ namespace AdamCore.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        [HttpGet]
+        public int MyAction()
+        {
 
+            return 666;
+        }
 
         public String[] ImageUploadGet()
         {
